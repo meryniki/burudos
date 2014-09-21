86c3c02225ddfdc512af33a9b4baf48f5e86b488
Meri <meryniki@gmail.com>
2014-09-21 20:44:44 -0300

    Agrego descripción de las columnas según el tipo de Archivo a subir.

--- web-app/js/mi.js
+++ web-app/js/mi.js
@@ -2,3 +2,58 @@ function showThinking() {
 	document.getElementById("thinking").style.display="block"; 	
 }
 
+function show_altas_desc() {
+	document.getElementById("desc_altas").style.display="block"; 	
+	document.getElementById("desc_bajas").style.display="none";
+	document.getElementById("desc_facturacion").style.display="none";
+	document.getElementById("desc_postventas").style.display="none";
+	document.getElementById("desc_cater").style.display="none";
+	document.getElementById("desc_porta").style.display="none";
+}
+
+function show_bajas_desc() {
+	document.getElementById("desc_bajas").style.display="block"; 
+	document.getElementById("desc_altas").style.display="none";
+	document.getElementById("desc_facturacion").style.display="none";
+	document.getElementById("desc_postventas").style.display="none";
+	document.getElementById("desc_cater").style.display="none";
+	document.getElementById("desc_porta").style.display="none";
+}
+
+function show_facturacion_desc() {
+	document.getElementById("desc_facturacion").style.display="block"; 	
+	document.getElementById("desc_altas").style.display="none";
+	document.getElementById("desc_bajas").style.display="none";
+	document.getElementById("desc_postventas").style.display="none";
+	document.getElementById("desc_cater").style.display="none";
+	document.getElementById("desc_porta").style.display="none";
+}
+
+function show_postventas_desc() {
+	document.getElementById("desc_postventas").style.display="block"; 
+	document.getElementById("desc_altas").style.display="none";
+	document.getElementById("desc_bajas").style.display="none";
+	document.getElementById("desc_facturacion").style.display="none";
+	document.getElementById("desc_cater").style.display="none";
+	document.getElementById("desc_porta").style.display="none";	
+}
+
+function show_cater_desc() {
+	document.getElementById("desc_cater").style.display="block"; 
+	document.getElementById("desc_altas").style.display="none";
+	document.getElementById("desc_bajas").style.display="none";
+	document.getElementById("desc_facturacion").style.display="none";
+	document.getElementById("desc_postventas").style.display="none";
+	document.getElementById("desc_porta").style.display="none";		
+}
+
+function show_porta_desc() {
+	document.getElementById("desc_porta").style.display="block"; 	
+	document.getElementById("desc_altas").style.display="none";
+	document.getElementById("desc_bajas").style.display="none";
+	document.getElementById("desc_facturacion").style.display="none";
+	document.getElementById("desc_postventas").style.display="none";
+	document.getElementById("desc_cater").style.display="none";		
+}
+
+
